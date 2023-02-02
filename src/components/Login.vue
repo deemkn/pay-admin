62d6d9c0f1d97d14a3f4515e8d69c1400631f262
chang pei shan <kim_mkn@hotmail.com>
2023-02-02 18:40:13 +0800

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login">
+        <div class="loginContainer">
+            <div>
+                帳號
+                <input/>
+            </div>
+            <div>
+                密碼
+                <input/>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="css" scoped>
+.login {
+    @apply
+    bg-neutral-800;
+}
+</style>
